b255766c212ff19b5f8c352fdb3dc8d94a15821b
Tommy Morriss <thmorriss@gmail.com>
2020-07-19 18:45:56 -0400

    made algorithm work slightly better, not perfect

--- shrink.js
+++ shrink.js
@@ -1,9 +1,9 @@
 document.body.style.visible = "hidden";
 
-
+numberToKeep = 1000
 // node->word count.
 words = []
-numberToKeep = 3;
+costs = []
 
 function recursiveCount(node) {
 	//console.log(node.style.visibility)
@@ -13,18 +13,35 @@ function recursiveCount(node) {
 	}
 }
 
+function nodeText(n) {
+	return [].reduce.call(n.childNodes, function(a, b) { return a + (b.nodeType === 3 ? b.textContent : ''); }, '');
+}
+
+function nodeCost(n) {
+	// TODO improve this, so that it is more efficient, or works at all????
+
+	// console.log('test')
+	// Returns the text content as a string
+	var sng = nodeText(n)
+	//console.log('----')
+	//console.log(string.length)
+	//console.log(node.textContent.length)
+	return sng.length;
+}
+
 
 function removeSmall(node) {
 	if(words.length < 10) {
 		console.log("firstrun")
 		// if we have not already filled out this array
 		recursiveCount(node);
-		words.sort(function(sn) { return - sn.textContent.length })
+		words.sort(function(a,b) { return nodeCost(b) - nodeCost(a)})
 		for (word of words) { word.backup_style = word.style; }
 
 		for (var i = 0; i < 10; i++) {
 			console.log(words[i])
 		}
+		numberToKeep = words.length;
 	}
 
 	//console.log(words)
@@ -54,10 +71,11 @@ window.addEventListener("keydown", function(event) {
 		console.log('zooming to level' + numberToKeep);
 		numberToKeep -= 10;
 		removeSmall(document.body)
+		event.preventDefault()
 	}
 
 	if (numberToKeep < 0) {
 		numberToKeep = 0
 	}
 })
-						
+
